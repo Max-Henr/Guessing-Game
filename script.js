91f2cd3f42b43760ddb44895e73ade12ca98f2aa
Max Henriksson <maxipod.henriksson@gmail.com>
2024-10-09 14:27:28 +0200

    added different modes

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const form = document.querySelector('#form');
+const mode = document.querySelector('.mode')
 let error = document.querySelector('.shaking-box')
 let playAgain = document.querySelector('.play-again')
 let g = 0;
+let i = 0
 let randomNumber = Math.round(Math.random() * 100)
+let hardMode = false
 console.log(randomNumber)
 
 form.addEventListener('submit', (e) => {
@@ -13,7 +16,6 @@ form.addEventListener('submit', (e) => {
     inputElement.value = '';
     error.style.display = 'none';
     if(numberValue < 1 || numberValue > 100){
-        // document.querySelector('.output').innerHTML = "enter a valid number"
         error.style.display = 'flex';
         return
     }
@@ -32,10 +34,33 @@ form.addEventListener('submit', (e) => {
         }
     }
     else{
-        // document.querySelector('.output').innerHTML = "enter a valid number"
         error.style.display = 'flex';
         return
     }
     g = g + 1
     document.querySelector('.guesses').innerHTML = g
+    i = i + 1
+    if (hardMode && i > 4) {
+        document.querySelector('.output').innerText = `Wrong! The number was ${randomNumber}`;
+        inputElement.disabled = true;
+        guessBtn.disabled = true;
+        playAgain.style.display = 'block';
+    }
 })
+
+mode.addEventListener('click', (e) => {
+    e.preventDefault()
+    if(hardMode){
+        mode.innerHTML = 'Switch to Hard Mode'
+        mode.style.backgroundColor = 'red'
+        document.querySelector('.description').innerHTML = 'Make a guess between 1 - 100'
+        hardMode = false
+    }
+    else{
+        mode.innerHTML = 'Switch to Easy Mode'
+        mode.style.backgroundColor = '#3498db'
+        document.querySelector('.description').innerHTML = 'Make a guess between 1 - 100 </br> you only have 5 tries'
+        hardMode = true
+    }
+})
+
